9653d92390a6c70b53d815ec2eb4b97be852ebc1
JB <jburns@chapman.edu>
2018-10-31 14:11:19 -0700

    changing only navbar text

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -31,10 +31,10 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'product-design-sprint', label: 'Product Design Sprint'},
-    {doc: 'doc4', label: 'API'},
-    {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    {doc: 'product-design-sprint', label: 'Connect'},
+    {doc: 'doc4', label: 'UX'},
+    {page: 'help', label: 'Design'},
+    {blog: '', label: 'Develop'},
   ],
 
   // If you have users set above, you add it here:
