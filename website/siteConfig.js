54e47a7f2c236823fd1d1f535aee5e15d6761bc7
JB <jburns@chapman.edu>
2018-10-31 14:08:11 -0700

    updating siteconfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'test-site',
-  organizationName: 'facebook',
+  organizationName: 'User Experience Society',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -66,7 +66,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} User Experience Society`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
