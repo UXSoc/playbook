2384e03e7772bba65e9df7e27885a170b75963cc
hat <jburns@chapman.edu>
2019-03-11 10:56:22 -0700

    Commented on not_here.js

--- not_here.js
+++ not_here.js
@@ -1,3 +1,6 @@
+//Point of this script is to just be a cross-platform way to print an error
+//Since too many people have tried to run 'npm start' here
+
 console.log("ERROR: ");
 console.log("Don't run 'npm start' here, run it in website/ folder");
 console.log("You can do that with 'cd website/'");
